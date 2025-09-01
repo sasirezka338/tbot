ff6d5ff18d5189b0b9343c3a6c294aee3bd0dbbe
sasirezka338 <sasirezka1@outlook.com>
2025-09-01 19:47:08 +0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,7 +5,7 @@
 // - env vars: TELEGRAM_TOKEN, BOT_SECRET (base64 or raw), GITHUB_TOKEN (optional),
 //             REPO_OWNER, REPO_NAME, WORKFLOW_ID, ALLOWED_USERS
 
-const TELEGRAM_TOKEN = TELEGRAM_TOKEN; // provided by Wrangler environment
+const TELEGRAM_TOKEN = "5391613004:AAEnMcQSprr_kly0_wlNvKKBvlCN6sPyGu4"; // provided by Wrangler environment
 const BOT_SECRET_RAW = "QV3eVZkUvIpBiO9GeLqfhFmBw5mciHAxqh3is5G7CDs=";     // base64 or raw, provided by Wrangler secrets
 const GITHUB_TOKEN_GLOBAL = "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" || null;
 const OWNER = "sasirezka338";
